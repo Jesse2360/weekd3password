0ea474901daca3a0cd7fe7362335563c5b0a31b7
jesse ceniceros <JCeniceros-10@yahoo.com>
2021-03-20 16:07:38 -0500

    added 2 staments 1. make sure length chosen is a # 2.Passw to be at least 8 characters in length.

--- script.js
+++ script.js
@@ -4,18 +4,31 @@ var specialCharacters = [  '@','%','+', '\\',"'",'!','#','$','^','?',':',',',')'
 // added arr of # for password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-//added arr of lowercase letters
+//added arr of lowercase letters for password
 var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
  'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
- // added arr of uppercase letters
+ // added arr of uppercase letters for password
 var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z'];
 
+// tell user for password option
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+  
+  // statement to see if the length of passw isa number nothing else
+  //statement to see if at least 8 characters in length
+  if (isNaN(length) === true) {
+    alert('Password length must be provided as a number');
+    return;
+  }
 
-
-
-
+  if (length < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
 
 
 
